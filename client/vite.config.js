02ee9f756dda29cc2454233e01d90c07b2dc0252
Ishan <ishannaik7@gmail.com>
2024-10-22 01:43:10 +0530

    Fixed deploy issue

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,17 @@
+// vite.config.js
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: "globalThis",
     "process.env": {},
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: "globalThis",
+      },
+    },
   },
 });
